ca9201c1ade17072cd3939c2318691f03cad6ce2
Kelsey Katie Sara Anethesia <PcVOXels@gmail.com>
2024-03-20 18:23:58 +0800

    working minecraft calculator
    minecraft calculator now counts for shulkers, stacks, remainder, logs

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using std::cout, std::cin, std::string;
+
+bool isInt(string input) {
+    if (input.length() < 1) {
+        return false;
+    }
+
+    for (int i = 0; i < input.length(); i++) {
+        if (input[i] == '-' && i == 0 && input.length() != 1) {
+            i++;
+        }
+
+        if (!isdigit(input[i])) {
+            if ((input[i] == '-' && i != 0) || (input[i] == '-' && input.length() == 1)) {
+                return false;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    cout << "\n\n\n\nMinecraft calculator\n\n\n\n";
+
+    string s_input;
+    float f_input, f_stacks, f_remainder, f_logs, f_shulkers;
+    bool isint = false;
+
+    // try {
+    //     cin >> input;
+    //     if (cin.fail())
+    //         throw std::invalid_argument("Input not a number.\n");
+    //     // if (input < 1)
+    //     //     throw 2;
+    // }
+    // catch (std::exception e) {
+    //     cout << "Input \"" << input << "\" is not a number.\n";
+    // }
+
+    while(!isint) {
+        cout << "Enter amount: ";
+        cin >> s_input;
+
+        isint = isInt(s_input);
+
+        if (stof(s_input) < 1) {
+            isint = false;
+        }
+    }
+
+    f_input = stof(s_input);
+
+    f_stacks = f_input/64;
+    f_remainder = (f_stacks - floor(f_stacks)) * 64;
+    f_logs = ceil(f_input/4);
+    f_shulkers = ceil(f_stacks/27);
+    
+
+    cout << "\n\nAmount: " << s_input 
+         << "\nShulkers: " << f_shulkers
+         << "\nStacks: " << int(f_stacks) 
+         << "\nRemainder: " << f_remainder
+         << "\nLogs: " << f_logs;
+
+    return 0;
+}
